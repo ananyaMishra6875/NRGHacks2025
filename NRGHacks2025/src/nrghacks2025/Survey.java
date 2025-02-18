0891dba23ab05729f7074bf95f8588e8e9185379
joykim <joykim@localhost>
2025-02-18 11:52:34 -0500

    switch panels

--- NRGHacks2025/src/nrghacks2025/Survey.java
+++ NRGHacks2025/src/nrghacks2025/Survey.java
@@ -1,5 +1,7 @@
 package nrghacks2025;
 
+import java.util.HashMap;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,6 +11,22 @@ package nrghacks2025;
  *
  * @author ananya,joy,emily,claire
  */
+
 public class Survey {
+    public HashMap<String, Integer> muscles = new HashMap<String, Integer>();
+    public Survey(){
+        muscles.put("leftArm", 0);
+        muscles.put("rightArm",0);
+        muscles.put("leftLeg",0);
+        muscles.put("rightLeg",0);
+        muscles.put("back",0);
+        muscles.put("abdominal",0);
+        muscles.put("chest",0);
+    }
     
+    public void updatePainLevel(String key){
+        int painLevel = muscles.get(key);
+        painLevel++;
+        muscles.replace(key, painLevel);
+    }
 }

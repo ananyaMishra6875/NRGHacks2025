4b75d27c1730cf12458e3c6badfc849ed0e0448f
ananyaMishra6875 <ananya.diva@gmail.com>
2025-02-18 09:33:46 -0500

    initial commit - ananya

---
+++ NRGHacks2025/src/nrghacks2025/HelperClass1.java
@@ -0,0 +1,16 @@
+package nrghacks2025;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ananya,joy,emily,claire
+ */
+public class HelperClass1 {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ NRGHacks2025/src/nrghacks2025/NRGHacks2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nrghacks2025;
+
+/**
+ *
+ * @author ananya,joy,emily,claire
+ */
+
+public class NRGHacks2025 {
+    public static void main(String[] args) {
+        
+    }
+}
